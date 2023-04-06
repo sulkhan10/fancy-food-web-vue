cde5acb506c00cfd93f5a7b7001a47bd1dfec34f
sulkhan10 <sulkhangalangsakti@gmail.com>
2023-04-06 21:11:25 +0700

    feat : add skeleton

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<script>
+import { RouterLink, RouterView } from "vue-router";
+import { mapState, mapActions } from 'pinia'
+  // import { useAnimeStore } from '../stores/anime'
+  // export default {
+  //   methods: {
+  //     ...mapActions(useAnimeStore, ['logoutHandler']),
+  //   },
+  //   computed: {
+  //     ...mapState(useAnimeStore, ['isLogin']),
+  //   }
+  // };
+</script>
+
+
+<template>
+  <div class="fixed z-10 w-full bg-gray-900">
+   
+  </div>
+</template>
+
